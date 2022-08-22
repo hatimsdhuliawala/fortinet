fe0fff31e6cb4bf0cbaa97c1f8d691753256f3e4
Hatim Dhuliawala <hdhuliawala@groupon.com>
2022-08-22 10:09:15 -0500

    date format update

--- src/components/OutageData/displayOutageData.js
+++ src/components/OutageData/displayOutageData.js
@@ -1,6 +1,7 @@
 'use strict';
 
-import './style.css'
+import './style.css';
+import moment from 'moment';
 
 const displayOutageData = (props) => {
   const {userData, nextPreviousPageClick, prevUrl, nextUrl} = props;
@@ -23,12 +24,12 @@ const displayOutageData = (props) => {
           <tbody>
             {userData.map((data, index) => {
               return (
-                <tr key={index}>
+                <tr key={index} className={data.end_time ? '' : 'ongoing-incident'}>
                   <td className="border-styling">{data.server_id || data.compound_service_id}</td>
                   <td className="border-styling">{data.server_name}</td>
-                  <td className="border-styling">{data.description ? data.description : 'No data Available'}</td>
-                  <td className="border-styling">{data.start_time}</td>
-                  <td className="border-styling">{data.end_time}</td>
+                  <td className="border-styling">{data.description}</td>
+                  <td className="border-styling">{data.start_time} {data.start_time && `(${moment(data.start_time).fromNow()})`}</td>
+                  <td className="border-styling">{data.end_time} {data.end_time && `(${moment(data.end_time).fromNow()})`}</td>
                   <td className="border-styling">{data.status}</td>
                   <td className="border-styling">{data.severity}</td>
                   <td className="border-styling">{data.has_active_maintenance.toString()}</td>
